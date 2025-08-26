4b01f128502ce3d758dcdd8cbde4a95b3e37eb93
Yukthamukhi Siddhoju <164024317+yuktham357@users.noreply.github.com>
2025-08-26 05:56:15 +0530

    Time: 0 ms (100.00%) | Memory: 22.4 MB (85.97%) - LeetSync

---
+++ 498-diagonal-traverse/diagonal-traverse.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> findDiagonalOrder(vector<vector<int>>& mat) {
+        int n=mat.size(),m=mat[0].size();
+        if(mat.empty() || mat[0].empty()) return {};
+        vector<int>ans(n*m);
+        int col=0,row=0;
+        for(int i=0;i<n*m;i++){
+            ans[i]=mat[row][col];
+            if((row+col)%2==0){
+                if(col==m-1) row++;
+                else if(row==0) col++;
+                else {
+                    row--;
+                    col++;
+                }
+            }
+            else{
+                if(row==n-1) col++;
+                else if(col==0) row++;
+                else{
+                    row++;
+                    col--;
+                }
+            }
+        }
+        return ans;
+    }
+};
