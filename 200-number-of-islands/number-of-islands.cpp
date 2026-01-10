80c4569dff1d0d088dd5437d5ba2006584d29e1e
Yukthamukhi Siddhoju <164024317+yuktham357@users.noreply.github.com>
2026-01-10 15:35:48 +0530

    Time: 81 ms (6.58%) | Memory: 46.8 MB (5.20%) - LeetSync

---
+++ 200-number-of-islands/number-of-islands.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int n=grid.size();
+        int m=grid[0].size();
+        vector<vector<bool>>vis(n,vector<bool>(m,false));
+        int count=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]=='1' && !vis[i][j]){
+                    count++;
+                    dfs(i,j,grid,vis);
+                }
+            }
+        }
+        return count;
+    }
+    void dfs(int i,int j,vector<vector<char>>& grid,vector<vector<bool>>&vis){
+        vis[i][j]=true;
+        vector<int>dr={1, 0, 0,-1,};
+        vector<int>dc={0, -1, 1,0};
+        for(int k=0;k<4;k++){
+            int r=i+dr[k];
+            int c=j+dc[k];
+            if(issafe(r,c,grid,vis)){
+                dfs(r,c,grid,vis);
+            }
+        }
+    }
+    bool issafe(int r,int c,vector<vector<char>>& grid,vector<vector<bool>>& vis){
+        int n=grid.size();
+        int m=grid[0].size();
+        return (r>=0 && r<n && c>=0 && c<m && grid[r][c]=='1' && !vis[r][c]);
+    }
+};
